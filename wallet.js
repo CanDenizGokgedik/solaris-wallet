95e14d2ddee6c4c68d43efc1b9a1167ae5268634
Aysaraa <candenizgokgedik@gmail.com>
2024-02-08 23:54:11 +0300

    multiple wallets integration

--- wallet.js
+++ wallet.js
@@ -11,16 +11,61 @@ const readline = require('readline')
 
 // Saving the Keypair to wallet.json.
 const saveWallet = (wallet) => {
-    fs.writeFileSync('wallet.json', JSON.stringify(wallet));
+    //fs.writeFile('wallet.json',wallet, (err)=>{console.log(err)});
+
+    fs.readFile('wallet.json', (err,data)=>{
+
+        if(err){
+            console.log(err);
+        }else{
+
+            let allData = [];
+            let tempData = {};
+            
+            // Checking for empty JSON file 
+            if(data != ''){
+                // If JSON file is filled, the data will parse.
+                tempData  = JSON.parse(data);
+            }
+            
+         
+            // Checking the JSON data include a single wallet or wallets array.
+            if(tempData[0] != null){
+                const dataLength = tempData.length;
+                console.log(dataLength)
+                // All wallets are pushing to the array.
+                for(let i=0; i<dataLength;i++){
+                    allData.push(tempData[i]);
+                }
+            }else{
+                // If JSON file include a single wallet,
+                // Checing the data is a eligible wallet format, 
+                // A eligible single wallet is pushing to the array.
+                if(tempData._keypair != null){
+                    allData.push(tempData)
+                }
+            }
+            // The last created wallet is pushing to the array.
+            allData.push(wallet);
+
+            // The array which including the all wallets data, writing into wallet.json.
+            fs.writeFile('wallet.json',JSON.stringify(allData), (err)=>{if(err){console.log(err)}});
+        }
+
+        
+
+    });
+
+    
 };
 
 // New Wallet is creating.
 const newWallet = () => {
-const wallet = new Keypair();
-saveWallet(wallet)
-const publicKey = new PublicKey(wallet._keypair.publicKey)
-const secretKey = wallet._keypair.secretKey
-console.log('The new wallet has been created.')
+    const wallet = new Keypair();
+    saveWallet(wallet)
+    const publicKey = new PublicKey(wallet._keypair.publicKey)
+    const secretKey = wallet._keypair.secretKey
+    console.log('The new wallet has been created.')
 }
 
 
